b2c44db8648ab2a7ebc965e942a19a00b4a2aa76
joshand <joshand@cisco.com>
2016-09-07 10:41:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!!"
+    page = "<form action='app.py'><br>\n"
+    page = page + "<h5>Submit your idea here!</h5><br>\n"
+    page = page + "Full Name: <input type='input' name='fullname'><br>\n"
+    page = page + "Company: <input type='input' name='company'><br>\n"
+    page = page + "Email Address: <input type='input' name='email'><br>\n"
+    page = page + "Idea: <textarea name='idea' rows='25' cols='100'></textarea><br>\n"
+    page = page + "<input type='submit' value='Submit'><br>\n"
+
+    return page
 
 
 if __name__ == "__main__":
